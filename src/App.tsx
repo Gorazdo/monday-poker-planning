319bd99f1bb3845a6f7bcb38aeb9a62afb44bdb2
Pavel Shchegolev <pavepy@gmail.com>
2021-04-29 11:18:23 +0200

    add cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import EditableHeading from "monday-ui-react-core/dist/EditableHeading";
 import { Avatar } from "./components/Avatar";
 import { Grid } from "./components/Grid";
 import { useBoardUsers } from "./hooks/useBoardUsers";
+import { PlayingCard } from "./components/PlayingCard";
 function App() {
   const { value: users } = useBoardUsers();
   console.log({ EditableInput, EditableHeading });
@@ -12,6 +13,13 @@ function App() {
       <EditableInput value="Edit H2 tooltip" tooltip="Click to edit" />
       <code>{JSON.stringify(users)}</code>
       <Button>Start planning</Button>
+      <Grid variant="row">
+        <PlayingCard value={3} label="3" variant="face" />
+        <PlayingCard value={3} label="3" variant="back" />
+        <PlayingCard value={3} label="3" variant="back" />
+        <PlayingCard value={100} label="100" variant="face" />
+        <PlayingCard value={1 / 2} label="1/2" variant="face" />
+      </Grid>
       <Grid variant="row">
         {users?.map((user) => (
           <Avatar key={user.id} url={user.photo_thumb} name={user.name} />
---
+++ src/components/PlayingCard/index.tsx
@@ -0,0 +1,76 @@
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import classes from "./index.module.css";
+
+export interface PlayingCardProps {
+  value: number;
+  label: string;
+  face?: string;
+  back?: string;
+  variant: "face" | "back";
+}
+const useAriaLabel = (variant, value) => {
+  if (variant === "back") {
+    return "The card is face down";
+  }
+  return `It's a ${value} card, faced up`;
+};
+
+const useButtonAriaLabel = (variant, value) => {
+  if (variant === "face") {
+    return "Select card";
+  }
+  return "Reveal card";
+};
+
+export const PlayingCard: React.FC<PlayingCardProps> = ({
+  value,
+  face,
+  back,
+  label,
+  variant,
+}) => {
+  const [state, setState] = useState(variant);
+  const [rotate, setRotate] = useState(variant === "face" ? 180 : 0);
+  const handleClick = (event) => {
+    if (state === "face") {
+      setState("back");
+      setRotate(rotate + 180);
+    } else {
+      setState("face");
+      setRotate(rotate + 180);
+    }
+  };
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+  return (
+    <div
+      className={classes.aspectRatio}
+      aria-label={useAriaLabel(state, value)}
+    >
+      <button
+        onClick={handleClick}
+        aria-label={useButtonAriaLabel(state, value)}
+        className={clsx(classes.root, classes.aspectRatioInner, {
+          [classes.rootFaceUp]: state === "face",
+          [classes.rootFaceDown]: state === "back",
+        })}
+        style={{ transform: `rotateY(${rotate}deg)` }}
+      >
+        <div
+          className={clsx(classes.content, classes.face)}
+          style={{ backgroundImage: `url(${face})` }}
+        >
+          <div className={classes.innerContent}>{label}</div>
+        </div>
+        <div
+          className={clsx(classes.content, classes.back)}
+          style={{ backgroundImage: `url(${back})` }}
+        >
+          <div className={classes.innerContent}></div>
+        </div>
+      </button>
+    </div>
+  );
+};
